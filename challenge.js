27ebbd0368032db46bd2f2394e346f4f1115c6cc
Faatah <guure781@gmail.com>
2024-12-21 17:10:13 +0300

    done exercise for tip-calculator

--- challenge.js
+++ challenge.js
@@ -1,2 +1,11 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+let bill=350
+let tip
+let total
+
+if(bill<=300 && (tip=bill*0.15) &&(total=bill+tip)){
+    console.log("The bill was "+bill , " the tip was "+tip, "and the total value "+total)
+}else if(bill>300 && (tip=bill*0.2)&&(total=bill+tip)){
+    console.log("The bill was "+bill , " the tip was "+tip, "and the total value "+total)
+}
